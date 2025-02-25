42785649332d28987352af602cc69f417d66ed7d
akira2000000 <angyg4lvez@gmail.com>
2025-02-25 11:04:50 -0600

    clase

---
+++ tarea1/script.js
@@ -0,0 +1,4 @@
+console.log("holaa consola");
+window.alert("AAAAAAAAAA");
+let nombre = "anggg";
+document.getElementById("nombre").innerHTML=nombre;
---
+++ tareas2/script.js
@@ -0,0 +1,5 @@
+let p1 ="parrafo 1";
+let p2 ="parrafo 2";
+
+document.getElementById('p1').innerHTML=p1;
+document.getElementById('p2').innerHTML=p2;
