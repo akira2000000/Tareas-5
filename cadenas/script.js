5af38e3d145891627dbf39918881964d33ce9e93
akira2000000 <angyg4lvez@gmail.com>
2025-03-11 10:51:31 -0600

    cadenas clase

---
+++ cadenas/script.js
@@ -0,0 +1,21 @@
+//variable
+let variable ="granadas cerezas frambuesas fresas manzanas";
+
+let variable2 = variable.toUpperCase();
+let variable3 = variable2.replace("MANZANAS", "MORAS");
+
+//document
+document.getElementById('cadena').innerHTML=variable;
+document.getElementById('cadena2').innerHTML=variable2;
+document.getElementById('cadena3').innerHTML=variable3;
+//consola
+
+console.log("holaaa gentita");
+window.alert("AAAAAAAAAAAAAA");
+console.log(variable.length);
+console.log(variable.at(4));
+
+
+
+
+
